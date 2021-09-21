54e83ff9904b283d22b2be05ae0d84db5b7aacda
luizrenatomf <luizrenatomf@gmail.com>
2021-09-21 18:13:04 -0300

    Adiciona ORM Django através de criação, seleção, alteração e exclusão de tarefa

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+
+from .models import Task
+from .forms import TaskForm
 
 def taskList(request):
-	return render(request,'tasks/list.html')
+	tasks = Task.objects.all().order_by('-created_at')
+	return render(request,'tasks/list.html',{'tasks':tasks})
 
 
-def yourName(request,name):
-	return render(request,'tasks/yourname.html',{'name':name})
+def taskView(request,id):
+	task = get_object_or_404(Task,pk=id)
+	return render(request,'tasks/task.html',{'task':task})
+
+
+def newTask(request):
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+
+		if form.is_valid():
+			task = form.save(commit=False)
+			task.done = 'doing'
+			task.save()
+			return redirect('/')
+	else:	
+		form = TaskForm()
+		return render(request, 'tasks/addtask.html',{'form':form})
+
+
+def editTask(request,id):
+	task = get_object_or_404(Task,pk=id)
+	form = TaskForm(instance=task)
+	if request.method == 'POST':
+		form = TaskForm(request.POST,instance=task)
+		if form.is_valid():
+			task.save()
+			return redirect('/')
+		else:
+			return render(request,'tasks/edittask.html',{'form':form,'task':task})
+	else:
+		return render(request,'tasks/edittask.html',{'form':form,'task':task})
+
+
+def deleteTask(request,id):
+	task = get_object_or_404(Task,pk=id)
+	task.delete()
+	messages.info(request,'Tarefa deletada com sucesso.')
+	return redirect('/')
