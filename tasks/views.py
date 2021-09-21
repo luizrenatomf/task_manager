4621ee57b55591159b06a1ee791eb2f8d060074a
luizrenatomf <luizrenatomf@gmail.com>
2021-09-21 18:51:42 -0300

    Adiciona funcionalidade de paginação e busca

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.contrib import messages
 
@@ -6,7 +7,14 @@ from .models import Task
 from .forms import TaskForm
 
 def taskList(request):
-	tasks = Task.objects.all().order_by('-created_at')
+	search = request.GET.get('search')
+	if search:
+		tasks = Task.objects.filter(title__icontains=search)
+	else:
+		tasks_list = Task.objects.all().order_by('-created_at')
+		paginator = Paginator(tasks_list,10)
+		page = request.GET.get('page')
+		tasks = paginator.get_page(page)
 	return render(request,'tasks/list.html',{'tasks':tasks})
 
 
--- todo/settings.py
+++ todo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'tasks',
     'about',
     'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
